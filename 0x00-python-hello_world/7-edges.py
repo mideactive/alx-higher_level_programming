e2fa732ac57bd939bc61e662e681d9dd8d81760d
mideactive <jenyoolumide@yahoo.com>
2022-11-29 02:08:14 +0000

    my work

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word1 = word[:3]
+word2 = word[7:]
+word3 = word[-8:-1]
+print(f"First 3 letters: {word1}")
+print(f"Last 2 letters: {word2}")
+print(f"Middle word: {word3}")
