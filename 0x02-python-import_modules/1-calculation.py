8a9479d22ede68cb5f64cdb8fda6d68dd2345895
mideactive <jenyoolumide@yahoo.com>
2022-12-01 15:36:54 +0000

    cal

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    from calculator_1 import *
+    a = 10
+    b = 5
+    add1 = add(a, b)
+    sub1 = sub(a, b)
+    mul1 = mul(a, b)
+    div1 = int(div(a, b))
+    print("{} + {} = {}".format(a, b, add1), end="\n")
+    print("{} + {} = {}".format(a, b, sub1), end="\n")
+    print("{} + {} = {}".format(a, b, mul1), end="\n")
+    print("{} + {} = {}".format(a, b, div1), end="\n")
