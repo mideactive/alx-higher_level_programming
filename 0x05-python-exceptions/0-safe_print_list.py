f14a81439ce01c777175f578590950ebdbca09e3
mideactive <jenyoolumide@yahoo.com>
2022-12-19 17:32:46 +0000

    list element

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    i = 0;
+    i = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
@@ -9,8 +9,3 @@ def safe_print_list(my_list=[], x=0):
             break
     print("")
     return (i)
-
-
-
-
-
