e7ce9d977503af23242d5ae7a72e6b6af9cf0a3c
mideactive <jenyoolumide@yahoo.com>
2022-12-21 01:43:20 +0000

    square Exception

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,8 +4,10 @@
 
 class Square:
     """Represent a square."""
+
     def __init__(self, size=0):
         """Initialize a new Square.
+
         Args:
             size (int): The size of the new square.
         """
