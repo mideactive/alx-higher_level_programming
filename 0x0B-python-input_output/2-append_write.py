b18b47d0cb96d3a7fffa508fa2ac8cdaca533c75
mideactive <jenyoolumide@yahoo.com>
2023-01-11 10:47:54 +0000

    append text

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -8,5 +8,5 @@ def append_write(filename="", text=""):
     with open(filename, mode='a', encoding='utf-8') as f:
         f.write(text)
     with open(filename, encoding='utf-8') as f:
-        l = len(f.read())
-    return (l)
+        ln = (len(f.read()))
+    return (ln)
