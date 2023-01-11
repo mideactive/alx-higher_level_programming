1c9ca352de339c6a614aaacc7661bfaeb050ff0b
mideactive <jenyoolumide@yahoo.com>
2023-01-11 10:45:01 +0000

    append text

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""2-append_write.py"""
+"""A function that appends a text to a file"""
+
+
 def append_write(filename="", text=""):
+    """Appends a text to a file"""
     with open(filename, mode='a', encoding='utf-8') as f:
         f.write(text)
     with open(filename, encoding='utf-8') as f:
