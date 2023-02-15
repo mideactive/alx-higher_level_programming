4871545d6bc26fe0f70ac14c293038dc5bdebf9d
mideactive <jenyoolumide@yahoo.com>
2023-02-15 13:57:55 +0000

    dump3

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,14 +2,12 @@
 -- run this first to import a SQL dump -->
 --      echo "CREATE DATABASE hbtn_0d_tvshows;" | mysql -uroot -p
 --      curl [link] -s | mysql -uroot -p hbtn_0d_tvshows
---
 -- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
 -- each record should display: tv_genres.name - number_shows
 -- don’t display a genre that doesn’t have any shows linked
 -- results must be sorted in descending order by the number of shows linked
 -- can use only one SELECT statement
 -- the database name will be passed as an argument of the mysql command
-
     SELECT tv_genres.name AS genre, COUNT(*) AS 'number_shows'
       FROM tv_genres
 INNER JOIN tv_show_genres
