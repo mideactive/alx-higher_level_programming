a436cf3108c3533448a580f6393416fd9210f2dd
mideactive <jenyoolumide@yahoo.com>
2023-02-15 14:04:28 +0000

    dump2

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
 -- run this first to import a SQL dump -->
---      echo "CREATE DATABASE hbtn_0d_tvshows;" | mysql -uroot -p
---      curl [link] -s | mysql -uroot -p hbtn_0d_tvshows
+-- echo "CREATE DATABASE hbtn_0d_tvshows;" | mysql -uroot -p
+-- curl [link] -s | mysql -uroot -p hbtn_0d_tvshows
 -- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
 -- each record should display: tv_genres.name - number_shows
 -- don’t display a genre that doesn’t have any shows linked
