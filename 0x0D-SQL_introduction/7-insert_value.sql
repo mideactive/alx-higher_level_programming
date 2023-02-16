4ff6dab82aa2c0c6ad87b5a73c1e3ce646b92182
mideactive <jenyoolumide@yahoo.com>
2023-02-16 11:50:15 +0000

    insert into table

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- A script that inserts a new row in the table first_table (database hbtn_0c_0) in MySQL server.
+-- id = 89, name = BEST School.
+
+INSERT INTO first_table VALUES(89, 'Best School');
