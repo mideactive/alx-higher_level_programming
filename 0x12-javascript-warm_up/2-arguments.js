8c2a6763d494d09e2ece041fd770465df665ed89
mideactive <jenyoolumide@yahoo.com>
2023-03-13 10:55:59 +0000

    process

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const cnt = process.argv.length;
-console.log(cnt === 2 ? 'No argument' : cnt === 3 ? 'Argument ound' : 'Arguments found');
+console.log(cnt === 2 ? 'No argument' : cnt === 3 ? 'Argument found' : 'Arguments found');
