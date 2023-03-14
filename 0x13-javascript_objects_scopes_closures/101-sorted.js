a4ad985475e0cf6392bdaf289c1e9fc810349763
mideactive <jenyoolumide@yahoo.com>
2023-03-14 23:42:23 +0000

    sorted

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-const dict = require('./text_files/101-data.js').dict;
-const newDict = {};
-for (const key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
-  }
-}
-console.log(newDict);
+const { dict } = require('./101-data');
+const myValue = Object.entries(dict).reduce((acc, [key, value]) => {
+  acc[value] = acc[value] ? [...acc[value], key] : [key];
+  return acc;
+}, {});
+console.log(myValue);
