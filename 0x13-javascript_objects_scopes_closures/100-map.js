73dec67ffb8eab221426e2e7122f5fdbe8f3197d
mideactive <jenyoolumide@yahoo.com>
2023-03-14 23:52:14 +0000

    map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 'use strict';
 
-const list = require('./text_files/100-data.js').list;
+const list = require('./100-data.js').list;
 console.log(list);
 const newList = list.map((x, index) => x * index);
 console.log(newList);
