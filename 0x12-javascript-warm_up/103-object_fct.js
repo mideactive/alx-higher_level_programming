d04a50afc9ea829426aa87ccdab54a9374af4b1c
mideactive <jenyoolumide@yahoo.com>
2023-06-13 21:54:47 +0000

    add function to increment an integer

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,18 +1,11 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12
-};
-
-console.log(myObject);
-
-Object.defineProperty(myObject, 'incr', {
-  value: function () {
+  value: 12,
+  incr: function () {
     this.value++;
-  },
-  enumerable: false,
-  writable: false
-});
+  }
+};
 
 myObject.incr();
 console.log(myObject);
