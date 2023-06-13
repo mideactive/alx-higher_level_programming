af9cef33acb93e744f399b7480b0ff47d86021a8
mideactive <jenyoolumide@yahoo.com>
2023-06-13 18:41:23 +0000

    print passed string

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -3,7 +3,7 @@ const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 
 if (arg1 === undefined || arg2 === undefined) {
-  console.log('Provide two arguments');
+  console.log(`${arg1} is ${arg2}`);
 } else {
   console.log(`${arg1} is ${arg2}`);
 }
