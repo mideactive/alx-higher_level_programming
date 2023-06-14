f7c3b926c95cc403911a11fb422af448aa5d5a50
mideactive <jenyoolumide@yahoo.com>
2023-06-14 20:47:52 +0000

    number of elements in a list

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,3 +1,11 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-}
+  let cn = 0;
+
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      cn++;
+    }
+  }
+  return cn;
+};
