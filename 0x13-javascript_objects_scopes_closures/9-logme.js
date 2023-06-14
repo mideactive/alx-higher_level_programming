5715d84b35e28b4b88cfeb03ceddeb9805c19093
mideactive <jenyoolumide@yahoo.com>
2023-06-14 21:03:42 +0000

    concatonate items

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
-let count = 0;
-exports.logMe = function (item) { console.log(`${count++}: ${item}`); };
+let cn = 0;
+
+exports.logMe = function (item) {
+  console.log(cn + ': ' + item);
+  cn++;
+};
