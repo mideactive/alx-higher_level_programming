bc0e2b7f4aa2a960fd0371464b8e385dbc50150b
mideactive <jenyoolumide@yahoo.com>
2023-06-14 20:18:52 +0000

    Square extends Rectangle

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-module.exports = class Square extends require('./4-rectangle.js') {
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
