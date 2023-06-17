5c5ce560ba3196956e77ccf1026d645d8e763a8e
mideactive <jenyoolumide@yahoo.com>
2023-06-17 22:11:46 +0000

    select states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,49 +1,16 @@
 #!/usr/bin/python3
 import MySQLdb
-import sys
+from sys import argv
 
 """ A script that lists all states from the database hbtn_0e_usa """
 
 
-def get_states(username, password, database):
-    # Connect to MySQL
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-
-    # Create  cursor object to interact with the database
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-
-    # Execute SQL query to retrieve states
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Fetch all rows from the result set
-    rows = cursor.fetchall()
-
-    # Close the cursor
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    for state in cursor.fetchall():
+        print(state)
     cursor.close()
-
-    # Close the database connection
     db.close()
-
-    # Print the states
-    for row in rows:
-        print(row)
-
-
-if __name__ == '__main__':
-    # Check if all three arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: python3 script.py <username> <password> <database>")
-    else:
-        # Retrieve the arguments
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database = sys.argv[3]
-
-        # Call the function to get the states
-        get_states(username, password, database)
