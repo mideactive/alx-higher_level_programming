28a115e5a40e131b5ed0e427f3e6ddf958240175
mideactive <jenyoolumide@yahoo.com>
2023-06-17 22:37:11 +0000

    filter state

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
 """A script that selects all state that starts with N from
 hbtn_0e_0_usa database"""
 
+import MySQLdb
+import sys
 
-def get_states(username, password, database):
-    # Connect to MySQL server
+if __name__ == '__main__':
+    h = 'localhost'
+    us = sys.argv[1]
+    pw = sys.argv[2]
+    dt = sys.argv[3]
+
     db = MySQLdb.connect(
-        host='localhost',
+        host=h,
         port=3306,
-        user=username,
-        passwd=password,
-        db=database
+        user=us,
+        passwd=pw,
+        db=dt
     )
 
     # Create a cursor object
@@ -21,29 +25,12 @@ def get_states(username, password, database):
     # Execute the SQL query to select states
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-    # Fetch all rows
+    # fetch rows
     rows = cursor.fetchall()
 
-    # Close the cursor
-    cursor.close()
-
-    # Close the database connection
-    db.close()
-
-    # Print the states
     for row in rows:
         print(row)
 
-
-if __name__ == '__main__':
-    # Check if all three arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: <username> <password> <database>")
-    else:
-        # Retrieve the arguments
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database = sys.argv[3]
-
-        # Call the function to get the states
-        get_states(username, password, database)
+    # closing cursor and db
+    cursor.close()
+    db.close()
