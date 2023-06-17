e741da0ca2bd102ea2f241585fc5766ddec8feaf
mideactive <jenyoolumide@yahoo.com>
2023-06-17 23:33:36 +0000

    filter cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
 """A script that filters cities in the
 hbtn_0e_4_usa db"""
 
+import MySQLdb
+import sys
 
-def get_cities_by_state(username, password, database, state_name):
+
+if __name__ == '__main__':
+    # Check if all four arguments are provided
+    if len(sys.argv) != 5:
+        print("Usage:<username> <password> <database> <state_name>")
+    else:
+        # Retrieve the arguments
+        un = sys.argv[1]
+        pw = sys.argv[2]
+        dt = sys.argv[3]
+        state_name = sys.argv[4]
+
     # Connect to MySQL server
     db = MySQLdb.connect(
         host='localhost',
         port=3306,
-        user=username,
-        passwd=password,
-        db=database
+        user=un,
+        passwd=pw,
+        db=dt
     )
 
     # Create a cursor object to interact with the database
@@ -28,25 +39,10 @@ def get_cities_by_state(username, password, database, state_name):
     # Fetch all rows from the result set
     rows = cursor.fetchall()
 
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
-
     # Print the cities
     for row in rows:
         print(row)
 
-
-if __name__ == '__main__':
-    # Check if all four arguments are provided
-    if len(sys.argv) != 5:
-        print("Usage:<username> <password> <database> <state_name>")
-    else:
-        # Retrieve the arguments
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database = sys.argv[3]
-        state_name = sys.argv[4]
-
-        # Call the function to get the cities by state
-        get_cities_by_state(username, password, database, state_name)
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
