dc94f0bc3d5a7d2ff26357780cfdc74a0dcd5680
mideactive <jenyoolumide@yahoo.com>
2023-06-18 04:58:27 +0000

    select all from stat using sqlalchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 """ A script that lists all states from hbtn_0e_6_usa db"""
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import sys
+from sys import argv
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == '__main__':
-    h = 'localhost'
-    u = sys.argv[1]
-    pas = sys.argv[2]
-    db_n = sys.argv[3]
-    p = 3306
-    eng = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
-          u, pas, h, p, db_n))
+if __name__ == "__main__":
+    engine = create_engine(
+                'mysql+mysqldb://{}:{}@localhost:3306/{}'
+                .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-    Session = sessionmaker(bind=eng)
-    session = Session().query(State).order_by(State.id)
-    for state in session:
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
+    session.close()
