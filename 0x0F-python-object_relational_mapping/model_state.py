c2b64c31d7b515071df2bb2f920cae407694e896
root <jenyoolumide@yahoo.com>
2023-06-19 05:39:13 -0700

    class state using sqlalchemy

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     import sys
 
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        'mysql+pymysql://{}:{}@localhost:3306/{}'.format(
             sys.argv[1], sys.argv[2], sys.argv[3]),
         pool_pre_ping=True
     )
