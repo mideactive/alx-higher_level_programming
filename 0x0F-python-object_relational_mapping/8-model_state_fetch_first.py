445bbdb245a72a589450c31a14da029c1507e53e
root <jenyoolumide@yahoo.com>
2023-06-19 05:56:05 -0700

    print first state using sqlalchemy

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that prints the first State object from the database hbtn_0e_6_usa.
+Script that prints the first State object from the database hbtn_0e_6_usa
 """
 import sys
 from sqlalchemy import create_engine
