ed1efdfdc2a0fd320c0aea3763fdfdcd094d6e12
root <jenyoolumide@yahoo.com>
2023-06-19 08:12:17 -0700

    update state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A script that update state"""
+
+
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    query = session.query(State).filter(State.id == 2)
+    query.first().name = 'New Mexico'
+    session.commit()
+    session.close()
