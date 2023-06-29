a05a5ee8903d0d265e0bec1e673eed5ebb8b0f61
root <jenyoolumide@yahoo.com>
2023-06-29 09:49:10 -0700

    display body size of url response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,15 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # A script that takes, send and display url size
 
-# Send a request to the URL and store the response body in a variable
 url=$1
 
-# Send a HEAD request to the URL and store the response header in a variable
-response_header=$(curl -sI "$url")
+# Send a GET request to the URL and store the response body in a variable
+response_body=$(curl -sL "$url")
 
-# Extract the Content-Length field from the response header
-content_length=$(echo "$response_header" | awk '/Content-Length/ {print $2}' | tr -d '\r')
+# Calculate the size of the response body in bytes
+content_length=$(echo -n "$response_body" | wc -c)
 
 # Display the size of the response body in bytes
 echo "$content_length"
-
