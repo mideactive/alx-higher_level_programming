a41c8e0df45ff8cd3e4a4da5b1cd535053a666e4
mideactive <jenyoolumide@yahoo.com>
2023-07-01 17:12:47 +0000

    requests error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A script that takes, sends and displays the url body"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    resp = requests.post(url)
+    if resp.status_code >= 400:
+        print("Error code:", resp.status_code)
+    else:
+        print(resp.text)
