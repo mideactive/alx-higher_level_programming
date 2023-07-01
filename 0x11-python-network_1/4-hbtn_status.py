c0ea535814c15b510eb018245162bc8d1e1e57bd
mideactive <jenyoolumide@yahoo.com>
2023-07-01 16:33:04 +0000

    status code with requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,9 +2,9 @@
 """A script that fetches https://alx-intranet.hbtn.io/status"""
 import requests
 
-if __name__ == '__main__':
-    url = 'https://alx-intranet.hbtn.io/status'
-    resp = requests.get('https://alx-intranet.hbtn.io/status')
-    print("Body response:")
-    print("\t- type:{}".format(type(resp.text)))
-    print("\t- content:{}".format(resp.text))
+
+url = 'https://alx-intranet.hbtn.io/status'
+resp = requests.get('https://alx-intranet.hbtn.io/status')
+print("Body response:")
+print("\t- type:", type(resp.text))
+print("\t- content:", (resp.text))
