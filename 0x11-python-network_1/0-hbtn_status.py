64626375d9a997a62ae1e3256efebe0d285fd3d4
mideactive <jenyoolumide@yahoo.com>
2023-07-01 13:23:59 +0000

    fetch url content

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,12 +4,12 @@ import urllib.request
 
 if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
-    resp = urllib.request.urlopen(url)
+    with urllib.request.urlopen(url) as req:
+        r = req.read()
 
-    r = resp.read()
     tp = type(r)
     cont = r.decode('utf-8')
-    print("Body response:$")
-    print("- type: {}$".format(r))
-    print("- content: {}$".format(tp))
-    print("- utf8 content: {}$".format(cont))
+    print("Body response:")
+    print("    - type: {}".format(tp))
+    print("    - content: {}".format(r))
+    print("    - utf8 content: {}".format(cont))
