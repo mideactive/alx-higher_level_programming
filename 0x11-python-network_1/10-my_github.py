187c4ea3bda58af8b0da5bfab43899d7d283051b
mideactive <jenyoolumide@yahoo.com>
2023-07-02 06:25:47 +0000

    extract user id from github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A script that gets my user ID from github"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = 'https://api.github.com/user'
+    resp = requests.get(url, auth=(username, password))
+
+    if resp.status_code == 200:
+        user_data = resp.json()
+        user_id = user_data['id']
+        print(user_id)
+    else:
+        print("none")
