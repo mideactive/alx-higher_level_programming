b472fe83f0edca361a1090554a463515738c9fbe
mideactive <jenyoolumide@yahoo.com>
2023-07-02 07:24:42 +0000

    get last 10 github commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,12 +4,9 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    resp = requests.get('https://api.github.com/repos/{}/{}/commits'
-                        .format(sys.argv[2], sys.argv[1]))
-    r_json = resp.json()
-    for i in range(len(r_json)):
-        if i >= 10:
-            break
-        diction = r_json[i]
-        print("{}: {}".format(diction['sha'],
-                              diction['commit']['author']['name']))
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    r = requests.get(url)
+    commits = r.json()
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
