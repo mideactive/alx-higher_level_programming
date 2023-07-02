4b56383e35b510c524a9e69b115c1a8167df0df7
mideactive <jenyoolumide@yahoo.com>
2023-07-02 07:19:39 +0000

    get last 10 github commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
 """Get github commits for the last 10 users"""
 import requests
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    repo = argv[1]
-    owner = argv[2]
-    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
-    r = requests.get(url)
-    if r.status_code == 200:
-        for i in r.json()[:10]:
-            print("{}: {}".format(
-                                  i.get("sha"),
-                                  i.get("commit").get("author").get("name")))
+    repository = sys.argv[1]  # Repository name
+    owner = sys.argv[2]  # Owner name
+
+    url = f"https://api.github.com/repos/{owner}/{repository}/commits"
+
+    # Send GET request to retrieve the commits
+    response = requests.get(url)
+
+    # Check the response status code
+    if response.status_code == 200:
+        commits = response.json()
+
+    # Iterate over the 10 most recent commits
+    for commit in commits[:10]:
+        sha = commit['sha']
+        author_name = commit['commit']['author']['name']
+        print(f"{sha}: {author_name}")
     else:
-        print("Error code: {}".format(r.status_code))
+        print("Error:", response.status_code)
