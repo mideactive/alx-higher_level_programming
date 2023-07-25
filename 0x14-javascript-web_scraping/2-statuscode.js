df95b852d18a674313b668c5c6939b6fbdc1f972
mideactive <jenyoolumide@yahoo.com>
2023-07-25 16:59:08 +0000

    display status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,15 +1,5 @@
 #!/usr/bin/node
-const request = require('axios');
-
-const url = process.argv[2];
-if (!url) {
-  console.error('Usage: node 2-statuscode.js <URL>');
-} else {
-  request.get(url, (error, response) => {
-    if (error) {
-      console.error(error);
-    } else {
-      console.log(`code: ${response.statusCode}`);
-    }
-  });
-}
+const request = require('request');
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});
