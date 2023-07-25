d13dc976e8ccd86a9063873341d6b40e8ac3498c
mideactive <jenyoolumide@yahoo.com>
2023-07-25 23:24:43 +0000

    number of tasks completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,20 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const url = process.argv[2];
+
 request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
+  if (error) {
+    console.error(error);
+  } else {
+    const data = JSON.parse(body);
+    const dictionary = {};
+
+    data.forEach(function (result) {
+      if (result.completed === true) {
+        const userId = result.userId;
+        dictionary[userId] = (dictionary[userId] || 0) + 1;
+      }
+    });
+
+    console.log(dictionary);
   }
 });
-
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
-    }
-  });
-}
