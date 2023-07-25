7d2626d748e6aff6c7c833058b4153634194882e
mideactive <jenyoolumide@yahoo.com>
2023-07-25 22:42:05 +0000

    print star wars character

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Usage: node 100-starwars_characters.js <movie-id>');
+} else {
+  const url = `https://swapi.dev/api/films/${movieId}/`;
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      const charactersUrls = JSON.parse(body).characters;
+      printCharacters(charactersUrls, 0);
+    } else {
+      console.error(`Error: Status code ${response.statusCode}`);
+    }
+  });
+}
+
+function printCharacters(charactersUrls, index) {
+  if (index >= charactersUrls.length) {
+    return;
+  }
+
+  const characterUrl = charactersUrls[index];
+  request(characterUrl, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      const characterName = JSON.parse(body).name;
+      console.log(characterName);
+    } else {
+      console.error(`Error: Status code ${response.statusCode}`);
+    }
+
+    // Recursively call printCharacters for the next character
+    printCharacters(charactersUrls, index + 1);
+  });
+}
