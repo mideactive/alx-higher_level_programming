b9d64a93ae7b9bd90fdc1ddc65cfa65c2a9e43a4
mideactive <jenyoolumide@yahoo.com>
2023-08-02 18:34:45 +0000

    List, add, remove

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,8 +1,11 @@
 $('document').ready(function () {
-  const url = 'https://www.fourtonfish.com/hellosalut/?';
-  $('INPUT#btn_translate').click(function () {
-    $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
-      $('DIV#hello').html(data.hello);
-    });
+  $('DIV#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+  $('DIV#remove_item').click(function () {
+    $('UL.my_list li:last').remove();
+  });
+  $('DIV#clear_list').click(function () {
+    $('UL.my_list').empty();
   });
 });
